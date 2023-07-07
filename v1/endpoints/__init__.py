9d0ad466c11f53edd6031bc79d9f750833ba6eb5
gradientivx <99917971+JakeXBT@users.noreply.github.com>
2023-07-07 19:13:10 +0400

    [rug-api] Add dummy chart data, remove `RealToken` entries

--- v1/endpoints/__init__.py
+++ v1/endpoints/__init__.py
@@ -2,8 +2,9 @@
 # from .liquidity import router as liquidity_router
 from .token import router as token_router
 from .search import router as search_router
-from .sourcecode import router as sourcecode_router
-from .tokenreel import router as tokenreel_router
+from .source import router as sourcecode_router
+from .reel import router as tokenreel_router
+from .chart import router as chart_router
 
 from fastapi import APIRouter
 
@@ -13,5 +14,6 @@ v1_router = APIRouter(prefix="/v1")
 # v1_router.include_router(liquidity_router, prefix="/liquidity", tags=["Liquidity Endpoints"])
 v1_router.include_router(token_router, prefix="/token", tags=["Token Endpoints"])
 v1_router.include_router(search_router, prefix="/search", tags=["Search Endpoints"])
-v1_router.include_router(sourcecode_router, prefix="/sourcecode", tags=["Source Code Endpoints"])
+v1_router.include_router(sourcecode_router, prefix="/source", tags=["Source Code Endpoints"])
 v1_router.include_router(tokenreel_router, prefix="/tokens", tags=["Token Reel Endpoints"])
+v1_router.include_router(chart_router, prefix="/chart", tags=["Chart Endpoints"])
