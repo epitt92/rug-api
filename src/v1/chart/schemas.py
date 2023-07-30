8fa3f1924c99247ef6b818e90bf412609d3f5992
gradientivx <99917971+JakeXBT@users.noreply.github.com>
2023-07-30 23:42:29 +0400

    [rug-api] Fix volume field, add chart fields to debug

--- src/v1/chart/schemas.py
+++ src/v1/chart/schemas.py
@@ -18,15 +18,26 @@ class ChartResponse(BaseModel):
     latestPrice: float
     latestReturn: float
     totalVolume: float = None
+    dayVolume: float = None
+    elapsedNumDays: float = None
+    avgCandleDurationMins: float = None
     data: List[ChartData]
 
     @root_validator(pre=True)
     def pre_process(cls, values):
         totalVolume = 0
+        dayVolume = 0
         for data in values['data']:
             if isinstance(data, dict):
-                totalVolume += data['volume'] * data['price']
+                totalVolume += data['volume']
+                if data['timestamp'] > values['timestampMax'] - 86400:
+                    dayVolume += data['volume']
             elif isinstance(data, ChartData):
-                totalVolume += data.volume * data.price
+                totalVolume += data.volume
+                if data.timestamp > values['timestampMax'] - 86400:
+                    dayVolume += data.volume
         values['totalVolume'] = totalVolume
+        values['dayVolume'] = dayVolume
+        values['elapsedNumDays'] = (values['timestampMax'] - values['timestampMin']) / 86400
+        values['avgCandleDurationMins'] = (values['timestampMax'] - values['timestampMin']) / (60 * values['numDatapoints'])
         return values
