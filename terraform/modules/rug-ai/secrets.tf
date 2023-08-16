ab6a5158d92410a91d36850f42cd5a3e348978bd
rawanb9 <rawanbannout9@gmail.com>
2023-08-16 09:52:00 +0300

    update secrets in ecs

--- terraform/modules/rug-ai/secrets.tf
+++ terraform/modules/rug-ai/secrets.tf
@@ -6,11 +6,11 @@ resource "aws_secretsmanager_secret" "ARBITRUM_BLOCK_EXPLORER_API_KEY" {
   name = "ARBITRUM_BLOCK_EXPLORER_API_KEY"
 }
 
-resource "aws_secretsmanager_secret" "BASE_BLOCK_EXPLORER_API_KEY " {
-  name = "BASE_BLOCK_EXPLORER_API_KEY "
+resource "aws_secretsmanager_secret" "BASE_BLOCK_EXPLORER_API_KEY" {
+  name = "BASE_BLOCK_EXPLORER_API_KEY"
 }
 
-resource "aws_secretsmanager_secret" "BNB_BLOCK_EXPLORER_API_KEY " {
-  name = "BNB_BLOCK_EXPLORER_API_KEY "
+resource "aws_secretsmanager_secret" "BNB_BLOCK_EXPLORER_API_KEY" {
+  name = "BNB_BLOCK_EXPLORER_API_KEY"
 }
 
