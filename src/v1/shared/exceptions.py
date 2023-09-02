732892efbbc1a92921699da57028940fcad61a12
gradientivx <99917971+JakeXBT@users.noreply.github.com>
2023-09-02 12:40:50 +0400

    [rug-api] Add `startswith` check to validate token address values

--- src/v1/shared/exceptions.py
+++ src/v1/shared/exceptions.py
@@ -3,3 +3,6 @@ from fastapi import HTTPException
 def validate_token_address(token_address: str):
     if len(token_address) != 42:
         raise HTTPException(status_code=400, detail=f"Token address {token_address} is not valid.")
+
+    if not token_address.startswith('0x'):
+        raise HTTPException(status_code=400, detail=f"Token address {token_address} is not valid.")
