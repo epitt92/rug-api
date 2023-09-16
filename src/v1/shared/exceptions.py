c588f72975c514f0990cdad2e3c9e60381892627
gradientivx <99917971+JakeXBT@users.noreply.github.com>
2023-09-16 18:40:49 +0400

    [rug-api] Define custom API exceptions

--- src/v1/shared/exceptions.py
+++ src/v1/shared/exceptions.py
@@ -2,7 +2,11 @@ from fastapi import HTTPException
 
 def validate_token_address(token_address: str):
     if len(token_address) != 42:
-        raise HTTPException(status_code=400, detail=f"Token address {token_address} is not valid.")
-
+        raise InvalidTokenAddressException(token_address)
     if not token_address.startswith('0x'):
-        raise HTTPException(status_code=400, detail=f"Token address {token_address} is not valid.")
+        raise InvalidTokenAddressException(token_address)
+
+
+class InvalidTokenAddressException(Exception):
+    def __init__(self, token_address: str):
+        super()._init__(token_address)
