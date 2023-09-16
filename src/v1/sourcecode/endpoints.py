5350ccf1a2732ae4c51c5ce32a479765f773a07c
gradientivx <99917971+JakeXBT@users.noreply.github.com>
2023-09-16 21:20:29 +0400

    [rug-api] Add error handling for source code endpoints

--- src/v1/sourcecode/endpoints.py
+++ src/v1/sourcecode/endpoints.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException, Depends
 import requests, json, time, dotenv, logging, os
 from botocore.exceptions import ClientError
+from pydantic import ValidationError
 
 from src.v1.shared.DAO import DAO
 from src.v1.shared.dependencies import get_primary_key
@@ -8,7 +9,7 @@ from src.v1.shared.models import validate_token_address
 from src.v1.sourcecode.schemas import SourceCodeResponse, SourceCodeFile
 
 from src.v1.shared.models import ChainEnum
-from src.v1.shared.exceptions import DatabaseLoadFailureException, DatabaseInsertFailureException, UnsupportedChainException
+from src.v1.shared.exceptions import DatabaseLoadFailureException, DatabaseInsertFailureException, UnsupportedChainException, BlockExplorerDataException, OutputValidationError
 
 router = APIRouter()
 
@@ -33,6 +34,14 @@ async def fetch_raw_code(chain: ChainEnum, token_address: str = Depends(validate
 
     prefix = os.environ.get(f'{_chain.upper()}_BLOCK_EXPLORER_URL')
     api_key = os.environ.get(f'{_chain.upper()}_BLOCK_EXPLORER_API_KEY')
+
+    if not prefix:
+        logging.error(f"The environment variable '{_chain.upper()}_BLOCK_EXPLORER_URL' was not found.")
+        raise Exception(f"The environment variable '{_chain.upper()}_BLOCK_EXPLORER_URL' was not found.")
+    
+    if not api_key:
+        logging.error(f"The environment variable '{_chain.upper()}_BLOCK_EXPLORER_API_KEY' was not found.")
+        raise Exception(f"The environment variable '{_chain.upper()}_BLOCK_EXPLORER_API_KEY' was not found.")
     
     payload = {
         'module': 'contract',
@@ -41,16 +50,23 @@ async def fetch_raw_code(chain: ChainEnum, token_address: str = Depends(validate
         'apikey': api_key
     }
 
-    response = requests.get(prefix, params=payload)
-    data = response.json()
+    try:
+        response = requests.get(prefix, params=payload)
+        data = response.json()
+    except Exception as e:
+        logging.error(f"Exception: During the call to GET getsourcecode from the Block Explorer for token {token_address} on chain {chain}: {e}")
+        raise BlockExplorerDataException(chain=chain, token_address=token_address)
 
-    logging.info(f'Fetched raw source code for {token_address} on chain {chain}')
-    data = response.json().get('result')[0]
-    
-    source = data.get('SourceCode')
+    logging.debug(f'Fetched raw source code for {token_address} on chain {chain}.')
 
-    logging.info(f'Fetched source code for {token_address} on chain {chain}')
+    try:
+        data = data.get('result')[0]
+        source = data.get('SourceCode')
+    except Exception as e:
+        logging.error(f"Exception: An error occurred whilst attempting to handle the `data` object: {data}. Exception: {e}")
+        return None
 
+    logging.debug(f'Fetched source code for {token_address} on chain {chain}.')
     return source
 
 
@@ -60,22 +76,30 @@ async def parse_raw_code(source: str) -> dict:
         clean_source = source.replace('{{', '{').replace('}}', '}').replace('\r', '').replace('\n', '')
 
         # Parse the clean_source string into a dictionary
+
+        # TODO: Raise suitable exception here
         try:
             parsed_source = json.loads(clean_source)
             return parsed_source.get('sources')
         except Exception as e:
-            raise HTTPException(status_code=500, detail=f"Failed to parse source code file. Error: {e}")
+            logging.error(f"Exception: While loading `clean_source` into a JSON object: {clean_source}. Exception: {e}")
+            raise Exception(f"Exception: While loading `clean_source` into a JSON object: {clean_source}. Exception: {e}")
     else:
         return source
 
 
 async def get_source_code_map(chain: ChainEnum, token_address: str = Depends(validate_token_address)):
-    logging.info(f'Fetching source code map for {token_address} on chain {chain}, via map...')
-    source = await parse_raw_code(await fetch_raw_code(chain, token_address))
-    logging.info(f'Raw souce code: {source}')
+    logging.debug(f'Fetching source code map for {token_address} on chain {chain}, via map...')
+
+    try:
+        source = await parse_raw_code(await fetch_raw_code(chain, token_address))
+    except Exception as e:
+        logging.error(f"Exception: During nested call to fetch and parse source code for {token_address} on chain {chain}: {e}")
+        raise Exception(f"Exception: During nested call to fetch and parse source code for {token_address} on chain {chain}: {e}")
 
     if isinstance(source, str):
         if len(source) == 0:
+            logging.debug(f"No source code was found. Returning an empty response.")
             return {'Flattened Source Code': 'No source code available for this contract.'}
         return {'Flattened Source Code': source}
     elif isinstance(source, dict):
@@ -85,7 +109,8 @@ async def get_source_code_map(chain: ChainEnum, token_address: str = Depends(val
         if len(new_keys) == 0 and len(source.keys()) == 1:
             return {'Flattened Source Code': source.get(list(source.keys())[0]).get('content')}
         elif len(new_keys) == 0:
-            raise HTTPException(status_code=500, detail=f"Failed to fetch raw source code file, or no source code available for this contract.")
+            logging.error(f"Exception: The new keys list was empty for token {token_address} on chain {chain}.")
+            raise Exception(f"Failed to fetch raw source code file, or no source code available for this contract.")
 
         key_map = dict(zip(source.keys(), new_keys))
         return {key_map.get(key): source.get(key).get('content') for key in source.keys()}
@@ -105,11 +130,19 @@ async def get_source_code(chain: ChainEnum, token_address: str = Depends(validat
     logging.debug(f'Fetching source code for {token_address} on chain {chain} from the database...')
 
     # Attempt to fetch transfers from DAO object
-    _source_code_map = SOURCE_CODE_DAO.find_most_recent_by_pk(pk)
-    
+    try:
+        _source_code_map = SOURCE_CODE_DAO.find_most_recent_by_pk(pk)
+    except ClientError as e:
+        logging.error(f"Exception: Boto3 exception whilst fetching data from 'sourcecode' with PK: {pk}")
+        raise DatabaseLoadFailureException()
+    except Exception as e:
+        logging.error(f"Exception: Unknown exception whilst fetching data from 'sourcecode' with PK: {pk}")
+        logging.error(f"Exception: {e}")
+        raise DatabaseLoadFailureException()
+
     # If source code is found, return it
     if not _source_code_map:
-        logging.info(f'No source code found for {token_address} on chain {chain}. Fetching from API...')
+        logging.debug(f'No source code found for {token_address} on chain {chain}. Fetching from API...')
         _source_code_map = await get_source_code_map(chain.value, token_address)
 
         # Write source code map to DAO file
@@ -119,23 +152,37 @@ async def get_source_code(chain: ChainEnum, token_address: str = Depends(validat
                 SOURCE_CODE_DAO.insert_one(partition_key_value=pk, item={'timestamp': int(time.time()), 'sourcecode': _source_code_map})
             except ClientError as e:
                 if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
-                    logging.warning(f'Source code map for {token_address} on chain {chain} already exists in DAO.')
-                    logging.warning(f'We do not allow duplicate writes for source code, since it is immutable.')
-                    raise e
+                    logging.error(f'Exception: Source code map for {token_address} on chain {chain} already exists in DAO.')
+                    logging.error(f'Exception: We do not allow duplicate writes for source code, since it is immutable.')
                 else:
-                    logging.warning(f'An unknown boto3 exception occurred while writing source code map for {token_address} on chain {chain} to DAO: {e}')
-                    raise e
+                    logging.error(f'Exception: An unknown boto3 exception occurred while writing source code map for {token_address} on chain {chain} to DAO: {e}')
+                
+                raise DatabaseInsertFailureException()
             except Exception as e:
-                logging.warning(f'An unknown exception occurred while writing source code map for {token_address} on chain {chain} to DAO: {e}')
-                raise e
+                logging.warning(f'Exception: An unknown exception occurred while writing source code map for {token_address} on chain {chain} to DAO: {e}')
+                raise DatabaseInsertFailureException()
         else:
-            raise HTTPException(status_code=404, detail=f'No source code found for {token_address} on chain {chain.value}')
+            raise Exception(f'No source code found for {token_address} on chain {chain.value}')
     else:
-        logging.info(f'Found source code for {token_address} on chain {chain} in DAO.')
+        logging.debug(f'Found source code for {token_address} on chain {chain} in DAO.')
         _source_code_map = _source_code_map.get('sourcecode')
 
     output = []
-    for key, value in _source_code_map.items():
-        output.append(SourceCodeFile(name=key, sourceCode=value))
-
-    return SourceCodeResponse(files=output)
+    try:
+        for key, value in _source_code_map.items():
+            output.append(SourceCodeFile(name=key, sourceCode=value))
+    except ValidationError as e:
+        logging.error(f"Exception: A ValidationError was raised for SourceCodeFile: {e.json()}")
+        raise OutputValidationError()
+    except Exception as e:
+        logging.error(f"Exception: An uncaught exception occurred was raised for SourceCodeFile: {e}")
+        raise OutputValidationError()
+    
+    try:
+        return SourceCodeResponse(files=output)
+    except ValidationError as e:
+        logging.error(f"Exception: A ValidationError was raised for SourceCodeResponse: {e.json()}")
+        raise OutputValidationError()
+    except Exception as e:
+        logging.error(f"Exception: An uncaught exception occurred was raised for SourceCodeResponse: {e}")
+        raise OutputValidationError()
--- src/v1/sourcecode/schemas.py
+++ src/v1/sourcecode/schemas.py
@@ -4,7 +4,6 @@ from typing import List, Optional
 class SourceCodeFile(BaseModel):
     name: str
     sourceCode: str
-    fileUrl: HttpUrl = None
 
 class SourceCodeResponse(BaseModel):
     files: Optional[List[SourceCodeFile]] = None
