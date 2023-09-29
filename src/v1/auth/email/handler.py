0b78cf434d47d9d3968763ff45451e93b703988b
gradientivx <99917971+JakeXBT@users.noreply.github.com>
2023-09-29 15:46:33 +0400

    [rug-api] Cognito event handler OTP initial commit

--- src/v1/auth/email/handler.py
+++ src/v1/auth/email/handler.py
@@ -1,17 +1,53 @@
-import json
-import boto3
+import json, boto3, jinja2
 
 ses = boto3.client('ses')
 
+#########################################
+#                                       #
+#           Email Templates             #
+#                                       #
+#########################################
+
+def render_template(template, **kwargs):
+    templateLoader = jinja2.FileSystemLoader(searchpath="src/v1/auth/email/templates")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    templ = templateEnv.get_template(template)
+    return templ.render(kwargs)
+
+#########################################
+#                                       #
+#        Lambda Event Handler           #
+#                                       #
+#########################################
+
 def lambda_handler(event, context):
     if event['triggerSource'] == 'CustomMessage_SignUp':
-        event['response']['emailSubject'] = 'Welcome to Our Platform!'
-        event['response']['emailMessage'] = """
-        <html>
-        <body>
-        <p>Thank you for signing up with us!</p>
-        <p>Your verification code is <strong>{####}</strong>. Use this code to complete your registration.</p>
-        </body>
-        </html>
-        """
+        recipient = event['request']['userAttributes']['email']
+        username = event['username']
+        verification_code = event['request']['codeParameter']
+
+        subject = "Your One-Time Passcode"
+        body = render_template('otp.html', username=username, code=verification_code, url="https://rug.ai")
+
+        _ = ses.send_email(
+            Source="no-reply@rug.ai",
+            Destination={
+                'ToAddresses': [
+                    recipient,
+                ]
+            },
+            Message={
+                'Subject': {
+                    'Data': subject
+                },
+                'Body': {
+                    'Html': {
+                        'Data': body
+                    }
+                }
+            })
+
+        event['response']['emailSubject'] = subject
+        event['response']['emailMessage'] = body
+
     return event
--- src/v1/auth/email/render-template.py
+++ src/v1/auth/email/render-template.py
@@ -2,7 +2,7 @@ import jinja2
 
 def render_template(template, **kwargs):
     ''' renders a Jinja HTML template '''
-    templateLoader = jinja2.FileSystemLoader(searchpath="email-templates/")
+    templateLoader = jinja2.FileSystemLoader(searchpath="src/v1/auth/email/templates")
     templateEnv = jinja2.Environment(loader=templateLoader)
     templ = templateEnv.get_template(template)
     return templ.render(kwargs)
