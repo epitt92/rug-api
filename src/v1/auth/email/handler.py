4552f4c5330e20c24ac3bab6b83bd52011f7a1aa
gradientivx <99917971+JakeXBT@users.noreply.github.com>
2023-09-29 17:35:24 +0400

    [rug-api] Refactor search path to local templates path

--- src/v1/auth/email/handler.py
+++ src/v1/auth/email/handler.py
@@ -7,7 +7,7 @@ import jinja2
 #########################################
 
 def render_template(template, **kwargs):
-    templateLoader = jinja2.FileSystemLoader(searchpath="src/v1/auth/email/templates")
+    templateLoader = jinja2.FileSystemLoader(searchpath="/templates")
     templateEnv = jinja2.Environment(loader=templateLoader)
     templ = templateEnv.get_template(template)
     return templ.render(kwargs)
