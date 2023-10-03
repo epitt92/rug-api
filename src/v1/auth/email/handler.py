68e1265fe5c6ed17db544bb27178f6729ce54bcc
gradientivx <99917971+JakeXBT@users.noreply.github.com>
2023-10-03 16:03:54 +0400

    [rug-api] Add support for password reset and custom email sending

--- src/v1/auth/email/handler.py
+++ src/v1/auth/email/handler.py
@@ -1,4 +1,7 @@
 import jinja2
+import boto3
+
+ses = boto3.client('ses')
 
 #########################################
 #                                       #
@@ -34,13 +37,44 @@ def custom_message_forgot_password(event):
     recipient = event['request']['userAttributes']['email']
     reset_code = event['request']['codeParameter']
 
-    # TODO: Must incorporate the reset code into the link
     subject = "You've Requested to Reset Your Password"
-    body = render_template('reset-password.html', title=subject + f": {reset_code}", username=recipient, reset_link="https://rug.ai")
+    reset_link = f"https://rug.ai/reset-pwd?username={recipient}&code={reset_code}"
+    body = render_template('reset-password.html', title=subject, username=recipient, reset_link=reset_link)
 
     event['response']['emailSubject'] = subject
     event['response']['emailMessage'] = body
     return event
+
+
+def send_custom_message_confirm_password_reset(event):
+    recipient = event['request']['userAttributes']['email']
+
+    subject = "You've Successfully Reset Your Password"
+    body = render_template('password-reset-successfully.html', title=subject, username=recipient, signin_link="https://rug.ai/signin")
+
+    email_subject = subject
+    email_body = body
+
+    ses.send_email(
+        Source="no-reply@rug.ai",
+        Destination={
+            'ToAddresses': [
+                recipient
+            ]
+        },
+        Message={
+            'Subject': {
+                'Data': email_subject
+            },
+            'Body': {
+                'Text': {
+                    'Data': email_body
+                }
+            }
+        }
+    )
+
+    return
 
 
 def lambda_handler(event, context):
@@ -50,6 +84,10 @@ def lambda_handler(event, context):
         return custom_message_sign_up(event)
     elif event['triggerSource'] == 'CustomMessage_ForgotPassword':
         return custom_message_forgot_password(event)
+    elif event['triggerSource'] == 'PostConfirmation_ConfirmForgotPassword':
+        # This trigger does not have email functionality by default
+        send_custom_message_confirm_password_reset(event)
+        return event
     # elif event['triggerSource'] == 'PostAuthentication_Authentication':
     #     return post_authentication(event)
     # elif event['triggerSource'] == 'PostPasswordChange_PasswordChange':
