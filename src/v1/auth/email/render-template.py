3e7973c775619bdebc13f3124681911c0f442439
Chuang Yu <cyu9960@gmail.com>
2023-10-03 12:01:11 +0800

    Fix styles and variables

--- src/v1/auth/email/render-template.py
+++ src/v1/auth/email/render-template.py
@@ -8,10 +8,45 @@ def render_template(template, **kwargs):
     return templ.render(kwargs)
 
 if __name__ == '__main__':
-    print(render_template('referral-invite.html', title="You've Been Invited!", username='John', referer='Jane', url='https://rug.ai'))
-    print(render_template('otp.html', title="OTP", code="123 456", url="https://Rug.ai"))
-    print(render_template('reset-password.html', title="You've Requested to Reset Your Password", username="RugEnjoyer", reset_link="https://Rug.ai"))
-    print(render_template('join-waitlist.html', title="Thanks for Joining the Waitlist!"))
-    print(render_template('welcome.html', title="Welcome to Rug.ai", url="https://Rug.ai/signup?ref=123456"))
-    print(render_template('airdrop.html', title="Your Weekly Airdrop", username="RugEnjoyer", points="24", total_points="284", rank="3,313", rank_out_of="50k", rank_delta="14", rank_direction="down", invites="36", points_distributed="248", total_users="157"))
-    print(render_template('password-reset-successfully.html', title="You've Successfully Reset Your Password", username="RugEnjoyer", signin_link="https://rug.ai/signin"))
+    print(render_template('referral-invite.html',
+                          title="You've Been Invited!",
+                          username='John',
+                          referer='Jane',
+                          url='https://rug.ai'))
+
+    print(render_template('otp.html',
+                          title="OTP",
+                          username="John",
+                          code="123 456",
+                          url="https://Rug.ai"))
+
+    print(render_template('reset-password.html',
+                          title="You've Requested to Reset Your Password",
+                          username="RugEnjoyer",
+                          reset_link="https://rug.ai/reset-pwd?username=xxxxx&code=123456"))
+
+    print(render_template('join-waitlist.html',
+                          title="Thanks for Joining the Waitlist!"))
+
+    print(render_template('welcome.html',
+                          title="Welcome to Rug.ai",
+                          url="https://Rug.ai/signup?ref=123456"))
+
+    print(render_template('airdrop.html',
+                          title="Your Weekly Airdrop",
+                          username="RugEnjoyer",
+                          total_points="284",
+                          points="24",
+                          points_direction="unchanged", # up, down, unchanged
+                          rank="3,313",
+                          rank_out_of="50k",
+                          rank_delta="14",
+                          rank_direction="down", # up, down, unchanged
+                          points_distributed="248",
+                          invites="36",
+                          total_users="157"))
+
+    print(render_template('password-reset-successfully.html',
+                          title="You've Successfully Reset Your Password",
+                          username="RugEnjoyer",
+                          signin_link="https://rug.ai/signin"))
